aa2a713fcd2841d4018d739cbdd5260dcaf6c80c
Purvi Gupta <singhalpurvi05@gmail.com>
2024-06-12 10:25:13 +0530

    changes in timer

--- timer-app/src/timer.js
+++ timer-app/src/timer.js
@@ -1,23 +1,45 @@
 import React, { useState, useEffect } from 'react';
+import './timer.css';
 
 function Timer() {
   const [seconds, setSeconds] = useState(0);
+  const [intervalId,setIntervalId] = useState(null);
+  const[isRunning,setIsRunning]=useState(false);
 
+  
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setSeconds(prevSeconds => prevSeconds + 1);
-    }, 1000);
 
-    return () => clearInterval(intervalId);
   }, []);
+ 
+  function handleStart(){
+    if(!isRunning){
+      const id = setInterval(() => {
+        setSeconds(prevSeconds => prevSeconds + 1);
+      }, 1000);
+      setIntervalId(id);
+      setIsRunning(true);
+    }
+    
+  }
+  
+  function handlePause(){
+    clearInterval(intervalId);
+    setIsRunning(false);
+  }
+
+  function handleReset(){
+    clearInterval(intervalId);
+    setSeconds(0);
+    setIsRunning(false);
+  }
 
   return (
     <div>
       <h1>Timer</h1>
       <p>{seconds} seconds</p>
-      <button>Start</button>
-      <button>Stop</button>
-      <button>Reset</button>
+      <button onClick={handleStart} className='start-button'>Start</button>
+      <button onClick={handlePause} className='pause-button'>Pause</button>
+      <button onClick={handleReset} className='reset-button'>Reset</button>
     </div>
   );
 }
